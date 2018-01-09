615000d92c7a6ea1f1e86d05658484fffedbeb12
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2018-01-09 10:49:49 +0100

    aspire sens in same calculation

--- aspire/AspirePoCalculator.m
+++ aspire/AspirePoCalculator.m
@@ -22,30 +22,32 @@ methods
     
     % override
     function iterativeCorrection(self, compl)
-        compl = self.removePo(compl);
-        combined = weightedCombinationAspire(compl, abs(compl));
-        phaseDiff = combined(:,:,:,2) .* conj(combined(:,:,:,1));
-        residual = combined(:,:,:,1) .* (conj(phaseDiff) ./ abs(phaseDiff));
-        residual(~isfinite(residual)) = 0;
-        poTerm = ones(size(residual));
-        
-        self.storage.write(compl(:,:,:,:,1), 'compl');
-        self.storage.write(abs(compl(:,:,:,:,1)), 'abscompl');
-        self.storage.write(residual, 'residualNaN');
-        self.storage.write(angle(combined), 'combined');
-        self.storage.write(phaseDiff, 'phaseDiff');
-        
-        for iStep = 1:self.iterativeSteps
-            residualSmooth = self.smoother.smooth(residual, abs(combined(:,:,:,1)));
-            poTerm = poTerm .* residualSmooth ./ abs(residualSmooth);
-            
-            self.storage.write(residual, ['residual' num2str(iStep)]);
-            self.storage.write(residualSmooth, ['residualSmooth' num2str(iStep)]);
-            self.storage.write(poTerm, ['poTerm' num2str(iStep)]);
-            
-            residual = residual .* (conj(residualSmooth) ./ abs(residualSmooth));
+        if self.iterativeSteps
+            compl = self.removePo(compl);
+            combined = weightedCombinationAspire(compl, abs(compl));
+            phaseDiff = combined(:,:,:,2) .* conj(combined(:,:,:,1));
+            residual = combined(:,:,:,1) .* (conj(phaseDiff) ./ abs(phaseDiff));
+            residual(~isfinite(residual)) = 0;
+            poTerm = ones(size(residual));
+
+            self.storage.write(compl(:,:,:,:,1), 'compl');
+            self.storage.write(abs(compl(:,:,:,:,1)), 'abscompl');
+            self.storage.write(residual, 'residualNaN');
+            self.storage.write(angle(combined), 'combined');
+            self.storage.write(phaseDiff, 'phaseDiff');
+
+            for iStep = 1:self.iterativeSteps
+                residualSmooth = self.smoother.smooth(residual, abs(combined(:,:,:,1)));
+                poTerm = poTerm .* residualSmooth ./ abs(residualSmooth);
+
+                self.storage.write(residual, ['residual' num2str(iStep)]);
+                self.storage.write(residualSmooth, ['residualSmooth' num2str(iStep)]);
+                self.storage.write(poTerm, ['poTerm' num2str(iStep)]);
+
+                residual = residual .* (conj(residualSmooth) ./ abs(residualSmooth));
+            end
+            self.addToPo(poTerm);
         end
-        self.addToPo(poTerm);
     end
     
     function addToPo(self, term)
--- aspire/AspireSensCalculator.m
+++ aspire/AspireSensCalculator.m
@@ -7,38 +7,52 @@ classdef AspireSensCalculator < AspirePoCalculator
 
     methods (Static)
         % override
-        function po = calculateAspirePo(compl, aspireEchoes, m)
-            if nargin == 2
-                m = 1;
-            end
-            po = calculateAspirePo@AspirePoCalculator(compl, aspireEchoes, m);
-            po = PoCalculator.removeMag(po);
-            mag = AspireSensCalculator.getMagAtTimeZero(compl(:,:,:,aspireEchoes,:), m);
-            po = mag .* po;
-        end
-        
-        function mag = getMagAtTimeZero(compl, m)
-            dim = size(compl);
-            mag = reshape(abs(compl(:,:,:,1,:)) .* (abs(compl(:,:,:,1,:)) ./ abs(compl(:,:,:,2,:))) .^ m, dim([1:3, 5]));
-            mag(~isfinite(mag)) = 0.1;
-        end
-        
 %         function po = calculateAspirePo(compl, aspireEchoes, m)
-%             if nargin == 1
-%                 aspireEchoes = [1 2];
+%             if nargin == 2
+%                 m = 1;
 %             end
-%             
-%             hip = calculateHip(compl, aspireEchoes);
-%             mag = sum(abs(compl(:,:,:,aspireEchoes(1),:)) ./ abs(compl(:,:,:,aspireEchoes(2),:)), 5);
-%             mag(~isfinite(mag)) = 0.1;
-%             hip = mag .* exp(1i * angle(hip));
-%             
-%             if nargin == 3
-%                 hip = hip .^ m;
-%             end
-% 
-%             po = AspirePoCalculator.subtractFromEcho(compl, hip, aspireEchoes(1));
+%             po = calculateAspirePo@AspirePoCalculator(compl, aspireEchoes, m);
+%             po = PoCalculator.removeMag(po);
+%             mag = AspireSensCalculator.getMagAtTimeZero(compl(:,:,:,aspireEchoes,:), m);
+%             po = mag .* po;
 %         end
+%         
+%         function mag = getMagAtTimeZero(compl, m)
+%             dim = size(compl);
+%             mag = reshape(abs(compl(:,:,:,1,:)) .* (abs(compl(:,:,:,1,:)) ./ abs(compl(:,:,:,2,:))) .^ m, dim([1:3, 5]));
+%             mag(~isfinite(mag)) = 0.1;
+%         end
+        
+        function po = calculateAspirePo(compl, aspireEchoes, m)
+            if nargin == 1
+                aspireEchoes = [1 2];
+            end
+            
+            echoDiff = AspireSensCalculator.calculateCombinedDifference(compl(:,:,:,aspireEchoes,:));
+            
+            if nargin == 3
+                echoDiff = echoDiff .^ m;
+            end
+            
+            dim = size(compl);
+            po = zeros([dim(1:3) dim(5)]);
+            for iCha = 1:size(po, 4)
+                po(:,:,:,iCha) = compl(:,:,:,aspireEchoes(1),iCha) .* echoDiff;
+            end
+        end
+        
+        function echoDiff = calculateCombinedDifference(compl)
+            dim = size(compl);
+            echoDiff = zeros(dim(1:3));
+            weightSum = zeros(dim(1:3));
+            for iCha = 1:size(compl, 5)
+                weight = abs(compl(:,:,:,1,iCha));
+                weightSum = weightSum + weight;
+                echoDiff = echoDiff + weight .* (compl(:,:,:,1,iCha) ./ compl(:,:,:,2,iCha));
+            end
+            echoDiff = echoDiff ./ weightSum;
+            echoDiff(~isfinite(echoDiff)) = 0;
+        end
         
     end
     
